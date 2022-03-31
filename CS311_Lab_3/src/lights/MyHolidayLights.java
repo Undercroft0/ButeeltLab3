833bf75024a342ea3bd6bd8734b9b6daf1562d48
Undercroft0 <86151720+Undercroft0@users.noreply.github.com>
2022-03-31 20:57:03 +0800

    hoy talaas asah , randomoor asah

--- CS311_Lab_3/src/lights/MyHolidayLights.java
+++ CS311_Lab_3/src/lights/MyHolidayLights.java
@@ -1,10 +1,53 @@
 package lights;
 
-public class MyHolidayLights {
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class MyHolidayLights implements HolidayLights  {
+
+	
+	ArrayList<Light> lightList = new ArrayList<Light>();
+
+	private int length;
+	
+	
+	public MyHolidayLights(int length) {
 		
+		this.length = length;
+
+		for(int i = 0; i < length; i++) {
+			
+			lightList.add(i, new ColoredLight(Color.green));	
+		} 
+	}
+	
+	int i = 0;
+	public List<Light> next() {
+		// TODO Auto-generated method stub
+		/*for(int i = 0; i < length; i++) {
+			ColoredLight cl1 = (ColoredLight) lightList.get(i);
+			cl1.randomChange();
+		}*/
+		
+		
+		ColoredLight cl1 = (ColoredLight) lightList.get(i);
+		
+		
+		cl1.setOn(true);
+			
+		ColoredLight cl2 = (ColoredLight) lightList.get(length-i-1);
+		
+		cl2.setOn(true);
+		if(i < length/2) {
+		i = i + 1;
+		}
+		
+		return lightList;
 	}
 
+	public int getLength() {
+		// TODO Temu
+		return length;
+	}
 }
